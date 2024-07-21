6fa2420b67f0fe8da824a768c35317dd3595017a
Samuele Ignesti <samuignesti@gmail.com>
2024-07-21 13:47:58 +0200

    commit smoothness in pymoo

--- Not Working/Main Pymoo.py
+++ Not Working/Main Pymoo.py
@@ -1,10 +1,10 @@
 import numpy as np
+import math
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
 from pymoo.core.problem import Problem
 from pymoo.algorithms.moo.nsga2 import NSGA2
 from pymoo.optimize import minimize
-from shapely.geometry import Point, Polygon
 
 class PathFindingProblem(Problem):
 
@@ -14,7 +14,7 @@ class PathFindingProblem(Problem):
         self.barriers = barriers
         self.n_points = n_points  # Number of points in the path excluding start and end
         super().__init__(n_var=2 * n_points,
-                         n_obj=2,
+                         n_obj=2,  # Change this to 3 objectives
                          xl=0.0,
                          xu=100.0)
     
@@ -25,10 +25,8 @@ class PathFindingProblem(Problem):
                 distance = np.linalg.norm(np.array(point) - np.array(vertex))
                 if distance < min_distance:
                     min_distance = distance
-        return -1 / 10*(min_distance + 1)  # Inverse penalty 
-        #return 1 / (min_distance)  # Inverse penalty 
-
-#############
+        return -1 / 100 * (min_distance + 1)  # Inverse penalty 
+    
     def point_in_polygon(self, point, polygon):
         x, y = point
         n = len(polygon)
@@ -47,16 +45,10 @@ class PathFindingProblem(Problem):
             p1x, p1y = p2x, p2y
 
         return inside
-
-    #def is_collision(self, point):
-        #for barrier in self.barriers:
-            #if self.point_in_polygon(point, barrier):
-                #return True
-        #return False
     
     def do_lines_intersect(self, A, B, C, D):
         def ccw(A, B, C):
-            return (C[1]-A[1]) * (B[0]-A[0]) > (B[1]-A[1]) * (C[0]-A[0])
+            return (C[1] - A[1]) * (B[0] - A[0]) > (B[1] - A[1]) * (C[0] - A[0])
         return ccw(A, C, D) != ccw(B, C, D) and ccw(A, B, C) != ccw(A, B, D)
 
     def is_collision(self, point1, point2=None):
@@ -65,14 +57,26 @@ class PathFindingProblem(Problem):
                 return True
             if point2 is not None:
                 for i in range(len(barrier)):
-                    if self.do_lines_intersect(point1, point2, barrier[i], barrier[(i+1) % len(barrier)]):
+                    if self.do_lines_intersect(point1, point2, barrier[i], barrier[(i + 1) % len(barrier)]):
                         return True
         return False   
+    
+    def smoothness(self, position):
+        smooth = 0
+        for i in range(1, len(position) - 1):
+            alpha = math.atan2(position[i + 1][0] - position[i][0], position[i + 1][1] - position[i][1])
+            beta = math.atan2(position[i][0] - position[i - 1][0], position[i][1] - position[i - 1][1])
+            if abs(alpha - beta) > math.pi / 8:
+                smooth += 100
+            smooth += abs(alpha - beta)
+        
+        return smooth
 
     def _evaluate(self, X, out, *args, **kwargs):
         paths = X.reshape(-1, self.n_points, 2)
         total_distances = []
         penalties = []
+        smoothness_scores = []
 
         for path in paths:
             path = np.vstack([self.start, path, self.end])
@@ -80,13 +84,15 @@ class PathFindingProblem(Problem):
             total_penalty = 0
 
             for i in range(len(path) - 1):
-                total_distance += np.linalg.norm(path[i+1] - path[i])
+                total_distance += np.linalg.norm(path[i + 1] - path[i])
                 total_penalty += self.calculate_penalty(path[i])
-                if self.is_collision(path[i]):
+                if self.is_collision(path[i], path[i + 1]):
                     total_penalty += 100  # Large penalty for collision
 
+            smooth = self.smoothness(path) 
             total_distances.append(total_distance)
             penalties.append(total_penalty)
+            #smoothness_scores.append(smooth)
 
         out["F"] = np.column_stack([total_distances, penalties])
 
@@ -116,7 +122,6 @@ barriers = [
     [(10, 10), (20, 10), (15, 20)],  # Triangle barrier
     [(30, 30), (50, 30), (50, 50), (30, 50)],  # Rectangle barrier
     [(70, 70), (80, 65), (85, 75), (75, 80)]  # Irregular quadrilateral barrier
-    #[(10,10), (80,80),(10, 85), (10,85)] #Irregular triangle
 ]
 
 # Define start and end points
